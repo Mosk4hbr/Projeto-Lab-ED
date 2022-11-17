c5ca84ecf93ef3123fff5e69b425a6082017814c
Lucas Alves <lucasbeeh3012@gmail.com>
2022-11-17 13:16:41 -0300

    Commit 17/11

--- labed.c
+++ labed.c
@@ -31,7 +31,7 @@ int main()
 {
     FILE *p1, *p2;
     TCabeca *prim = NULL;
-    int op = 1;
+    int op = 1, op3;
 	TCabeca *cabeca = criaLista();
     while (op != 5)
     {
@@ -50,7 +50,17 @@ int main()
             	//printf("%d %d\n", contY, contZ);
                 insereLista(cabeca, p1);
                 imprimeLista(cabeca->pY);
-        	}
+        	}break;
+            case 3:
+            {
+                printf("1-Pessoas posto Y\n 2-Pessoas posto Z\n 3-Pessoas posto YZ\n");
+                lerarquvios(op3, cabeca);
+
+            }break;
+            case 4:
+            {
+                desaloca(cabeca,p1,p2,p1);//Falta o arquivo unificado pra chamada da função free no lugar do p1
+            }break;
         }
     }
 
@@ -75,6 +85,7 @@ void insereLista(TCabeca *pprim, FILE *p)
         novo->pprox = aux;
         aux->pprox = NULL;
     }
+    return;
 }
 void imprimeLista(TPessoa *p)
 {
@@ -92,7 +103,40 @@ void imprimeLista(TPessoa *p)
     }
 }
 
-
+TPessoa *lerarquvios(int op3, TCabeca *cabeca)//opcao 3 ler listas
+{
+    scanf("%d", &op3);
+    switch(op3)
+    {
+        case 1:
+        {
+            for(TPessoa *tmp = cabeca->pY; tmp != NULL; tmp = tmp->pprox)
+            {
+                printf("%s %d %d %d %s\n", tmp->nome, tmp->idade, tmp->dose1, tmp->dose2, tmp->vacina);
+            }
+        }break;
+        case 2:
+        {
+            for(TPessoa *tmp = cabeca->pZ; tmp != NULL; tmp = tmp->pprox)
+            {
+                printf("%s %d %d %d %s\n", tmp->nome, tmp->idade, tmp->dose1, tmp->dose2, tmp->vacina);
+            }
+        }break;
+        case 3:
+        {
+            for(TPessoa *tmp = cabeca->pZ; tmp != NULL; tmp = tmp->pprox)//Aqui muda o posto para o unificado, no tmp = cabeca->pZ para tmp = cabeca->pYZ
+            {
+                printf("%s %d %d %d %s\n", tmp->nome, tmp->idade, tmp->dose1, tmp->dose2, tmp->vacina);
+            }
+        }break;
+        default:
+        {
+            printf("numero invalido, informe outro\n");
+            scanf("%d", &op3);
+        }
+    }
+    return NULL;
+}
 
 TPessoa *alocaNo(FILE *parquivo)
 {
@@ -155,4 +199,34 @@ int lerArquivoY(FILE *parquivo)
     free(temp);
     temp = NULL;
     return cont;
+}
+
+TCabeca *desaloca(TCabeca *Cabeca, TPessoa *p1, TPessoa *p2, TPessoa *p3)//opcao 4
+{
+    TPessoa *tmp = NULL, *tmp2 = NULL, *tmp3 = NULL;
+    while(p1)
+    {
+        tmp = p1->pprox;
+        free(p1);
+        p1 = tmp;
+        printf("Free p1\n");
+    }
+    printf("list 1 esvaziada ou lista vazia\n");
+    while(p2)
+    {
+        tmp = p2->pprox;
+        free(p2);
+        p2 = tmp;
+        printf("Free p2\n");
+    }
+    printf("list 2 esvaziada ou lista vazia\n");
+    while(p3)
+    {
+        tmp = p3->pprox;
+        free(p3);
+        p3 = tmp;
+        printf("Free p3\n");
+    }
+    printf("list 3 esvaziada ou lista vazia\n");
+    return NULL;
 }
